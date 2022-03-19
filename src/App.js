f074be54ecd74b0291f3c1ffa518dca381134e2a
dec-h1 <doracallisaya21@gmail.com>
2022-03-19 14:52:36 -0500

    react con css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react'
+import CreateTodoButton from './CreateTodoButton';
 import {TodoCounter} from './TodoCounter'
+import TodoItem from './TodoItem';
+import { TodoList } from './TodoList';
+import TodoSearch from './TodoSearch';
 //import logo from './logo.svg';
 // import './App.css';
-// const todos=[
-//   {text: 'Cortar cebolla', completed:false},
-//   {text: 'Tomar el curso de intro a React', completed:false},
-//   {text: 'Llorar con la llorona', completed:false}
+const todos=[
+  {text: 'Cortar cebolla', completed:true},
+  {text: 'Tomar el curso de intro a React', completed:false},
+  {text: 'Llorar con la llorona', completed:false},
+  {text: 'bienvenida la princesa de la robotica', completed:false}
 
-// ];
+];
 function App() {
   return (
    <React.Fragment>
     <TodoCounter/>
-       <h2>Has completado 2 de 3 Todos</h2>
-     {/*  <TodoSearch/> */}
-        <input placeholder='Cebolla'/>
-      {/* <TodoList>
+       {/* <h2>Has completado 2 de 3 Todos</h2> */}
+      <TodoSearch/>
+        {/* <input placeholder='Cebolla'/> */}
+      <TodoList>
         {todos.map(todo=>(
-          <TodoItem/>
+          <TodoItem 
+              key={todo.text} 
+              text={todo.text}
+              completed={todo.completed}
+              />
+
         ))}
-    </TodoList> */}
-  {/*   <CreateTodoButton/> */}
-    <button>+</button>
+    </TodoList>
+    <CreateTodoButton/>
+    {/* <button>+</button> */}
    </React.Fragment>
   );
 }
